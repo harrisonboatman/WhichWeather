d9bd8ece371f927d449d3bde08846ea5c54fafce
Harrison Boatman <hrry.boatman2@gmail.com>
2023-01-11 21:08:10 -0600

    added 5 day forecast boxes

--- assets/code.js
+++ assets/code.js
@@ -4,6 +4,21 @@ var today = dayjs();
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
 var humidity = document.querySelector('#humidity');
+var temp1 = document.querySelector('#day1temp');
+var wind1 = document.querySelector('#day1wind');
+var humidity1 = document.querySelector('#day1humidity');
+var temp2 = document.querySelector('#day2temp');
+var wind2 = document.querySelector('#day2wind');
+var humidity2 = document.querySelector('#day2humidity');
+var temp3 = document.querySelector('#day3temp');
+var wind3 = document.querySelector('#day3wind');
+var humidity3 = document.querySelector('#day3humidity');
+var temp4 = document.querySelector('#day4temp');
+var wind4 = document.querySelector('#day4wind');
+var humidity4 = document.querySelector('#day4humidity');
+var temp5 = document.querySelector('#day5temp');
+var wind5 = document.querySelector('#day5wind');
+var humidity5 = document.querySelector('#day5humidity');
 
 function handleSearchForm() {
     
@@ -31,6 +46,21 @@ function getWeather(lat,lon){
             var farTemp = weather.list[0].main.temp *(9/5) - 459.67;
             var windNum = weather.list[0].wind.speed;
             var humidityNum = weather.list[0].main.humidity;
+            // var farTemp1 = weather.list[2].main.temp *(9/5) - 459.67;
+            // var windNum1 = weather.list[2].wind.speed;
+            // var humidityNum1 = weather.list[2].main.humidity;
+            // var farTemp2 = weather.list[9].main.temp *(9/5) - 459.67;
+            // var windNum2 = weather.list[9].wind.speed;
+            // var humidityNum2 = weather.list[9].main.humidity;
+            // var farTemp3 = weather.list[17].main.temp *(9/5) - 459.67;
+            // var windNum3 = weather.list[17].wind.speed;
+            // var humidityNum3 = weather.list[17].main.humidity;
+            // var farTemp4 = weather.list[25].main.temp *(9/5) - 459.67;
+            // var windNum4 = weather.list[25].wind.speed;
+            // var humidityNum4 = weather.list[25].main.humidity;
+            // var farTemp5 = weather.list[33].main.temp *(9/5) - 459.67;
+            // var windNum5 = weather.list[33].wind.speed;
+            // var humidityNum5 = weather.list[33].main.humidity;
             farTemp = Math.trunc(farTemp);
             console.log(farTemp);
             weatherNow.textContent =  weather.city.name + " on " + today.format("MMM D, YYYY");
